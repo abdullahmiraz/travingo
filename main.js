08e088ffd60885856fdce7e63ac96c92d747fbdb
Abdullah Miraz <abdullahmiraz2015@gmail.com>
2023-06-25 17:29:40 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+// scroll reveal 
+
+window.sr = ScrollReveal();
+
+sr.reveal(".animate-left", {
+    origin:'left',
+    duration: 1000 ,
+    distance: '25rem',
+    delay: 300
+});
+
+sr.reveal(".animate-right", {
+    origin:'right',
+    duration: 1000 ,
+    distance: '25rem',
+    delay: 600
+});
+sr.reveal(".animate-top", {
+    origin:'top',
+    duration: 1000 ,
+    distance: '25rem',
+    delay: 600
+});
+
+sr.reveal(".animate-bottom", {
+    origin:'bottom',
+    duration: 1000 ,
+    distance: '25rem',
+    delay: 600
+});
